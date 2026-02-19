3f13a336fd01f260db53318a36753d585692fc79
deborq67 <debohorquez@mail.fhsu.edu>
2026-02-19 00:58:09 -0600

    Made a class capable of handling pie charts. Will expand on this.

--- main.py
+++ main.py
@@ -4,6 +4,28 @@ import os
 import sys
 import matplotlib.pyplot as plt
 
+
+'''This class handles most of the aesthetics of my graphs, meaning all I
+have to do is put in the mandatory information like title, x, and y fields.
+The class handles the longer decorative parts.'''
+
+
+class GraphFormat:
+    def __init__(self, df, x=None, y=None, labels=None):
+        self.df = df
+        self.x = x
+        self.y = y
+        self.labels = labels
+
+    # Get Top 10 items of a column and turn it into a pie chart.
+
+    def pie_chart_top_10(self):
+        top_10 = self.df[f'{self.x}'].value_counts().iloc[:10]
+        top_10.plot(kind="pie", colormap='tab20c')
+        plt.show()
+        return top_10
+
+# *series allows the field to have more than one argument if needed.
 def clean_df(data, *series, uniq=False):
     #Get the series string or strings.
     series=series[0]
@@ -26,7 +48,14 @@ if not os.path.isfile('occurrence.txt'):
 #It has both numbers and letters so treating the db as a string type is the best option for now.
 
 fish_database=pd.read_csv('occurrence.txt', sep='\t', dtype=str)
-fish_database_unique=fish_database.drop_duplicates(subset=['scientificName'])
-fish_database_unique=clean_df(fish_database_unique,'scientificName','country', uniq=True)
-print(fish_database_unique[['scientificName','country']])
+fish_database_unique=fish_database.dropna(subset=['scientificName'])
+fish_database_unique=clean_df(fish_database_unique,'scientificName', uniq=True)
+
+#Q1: What fish orders have the most species?
+
+'''For some taxonomical context, orders are big groups of organisms with common traits.
+  For example, butterflies and moths belong in the order Lepidoptera, catfish belong in
+  the order Siluriformes, etc.'''
+
+GraphFormat(fish_database_unique,'order').pie_chart_top_10()
 
